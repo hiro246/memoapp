38770d7c3a8a155c9bb2865c3f403c76e51db540
hiro246 <roki_stereo@yahoo.co.jp>
2020-06-25 20:19:56 +0900

    Create MemoListScreen Fix Format

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import Appbar from "./src/components/Appbar";
-import MemoList from "./src/components/MemoList";
-import CircleButton from "./src/elements/CircleButton";
+import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoList />
-        <CircleButton>+</CircleButton>
+        <MemoListScreen />
       </View>
     );
   }
@@ -20,9 +18,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFDF6",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#FFFDF6',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     paddingTop: 78,
   },
 });
--- src/elements/BodyText.js
+++ src/elements/BodyText.js
@@ -1,11 +1,13 @@
-import { StyleSheet, View, Text } from "react-native";
-import React from "react";
+import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
 
 class BodyText extends React.Component {
   render() {
     return (
       <View>
-        <Text style={styles.text}>{this.props.children}</Text>
+        <Text style={styles.text}>
+          {this.props.children}
+        </Text>
       </View>
     );
   }
@@ -13,8 +15,8 @@ class BodyText extends React.Component {
 
 const styles = StyleSheet.create({
   text: {
-    color: "#DDD",
-    backgroundColor: "#eee",
+    color: '#DDD',
+    backgroundColor: '#eee',
   },
 });
 
