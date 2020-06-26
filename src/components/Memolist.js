9b008dded5da1858ba49c00aa3f2fb3dbb7f83a7
hiro246 <roki_stereo@yahoo.co.jp>
2020-06-26 22:03:06 +0900

    Add icon font

--- src/components/Memolist.js
+++ src/components/Memolist.js
@@ -1,58 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-class MemoList extends React.Component {
+import MemoList from '../components/MemoList';
+import CircleButton from '../elements/CircleButton';
+
+class MemoListScreen extends React.Component {
   render() {
     return (
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
+      <View style={styles.container}>
+        <MemoList />
+        <CircleButton>
+          {'\uf067'}
+        </CircleButton>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  memoList: {
-    width: '100%',
+  container: {
     flex: 1,
-  },
-  memoListItem: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    backgroundColor: '#fff',
-  },
-  memoTitle: {
-    fontSize: 18,
-    marginBottom: 4,
-  },
-  memoDate: {
-    fontSize: 12,
-    color: '#a2a2a2',
+    width: '100%',
   },
 });
 
-export default MemoList;
+export default MemoListScreen;
