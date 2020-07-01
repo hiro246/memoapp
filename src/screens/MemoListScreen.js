cc6f4ee8e26849689c694ca89294eae54bb7f3bd
hiro246 <roki_stereo@yahoo.co.jp>
2020-07-01 13:43:38 +0900

    Fix MemoListScreen

--- src/screens/MemoListScreen.js
+++ src/screens/MemoListScreen.js
@@ -5,6 +5,11 @@ import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
 class MemoListScreen extends React.Component {
+  handlePress() {
+    const { params } = this.props.navigation.state;
+    this.props.navigation.navigate('MemoCreate', { currentUser: params.currentUser });
+  }
+
   render() {
     return (
       <View style={styles.container}>
